79936271d9bc1ff8c72097753048fcc69197717a
Chukkrit Visitsaktavorn <chukkrit.juk@gmail.com>
2021-08-18 17:23:09 +0700

    refactor: add alias path

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,57 @@
-import { resolve } from 'path'
-import { defineConfig } from 'vite'
+import { defineConfig, UserConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import react from 'vite-preset-react'
+
 // https://vitejs.dev/config/
-export default defineConfig({
+const config: UserConfig = {
   plugins: [react(), tsconfigPaths()],
   resolve: {
-    alias: [{ find: '@', replacement: resolve(__dirname, 'src') }],
+    alias: [
+      {
+        find: '@pages',
+        replacement: `${__dirname}/src/pages`
+      },
+      {
+        find: '@layouts',
+        replacement: `${__dirname}/src/layouts`
+      },
+      {
+        find: '@components',
+        replacement: `${__dirname}/src/components`
+      },
+      {
+        find: '@styles',
+        replacement: `${__dirname}/src/styles`
+      },
+      {
+        find: '@services',
+        replacement: `${__dirname}/src/services`
+      },
+      {
+        find: '@models',
+        replacement: `${__dirname}/src/models`
+      },
+      {
+        find: '@stores',
+        replacement: `${__dirname}/src/stores`
+      },
+      {
+        find: '@libs',
+        replacement: `${__dirname}/src/libs`
+      },
+      {
+        find: '@utils',
+        replacement: `${__dirname}/src/utils`
+      },
+      {
+        find: '@context',
+        replacement: `${__dirname}/src/context`
+      }
+    ]
   },
   build: {
-    sourcemap: true,
-  },
-})
+    sourcemap: true
+  }
+}
+
+export default defineConfig(config)
